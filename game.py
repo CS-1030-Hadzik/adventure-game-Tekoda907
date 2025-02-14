000bf180e901c76474488ed707d728793e6fe05a
takoda english <takodaenglish@mail.weber.edu>
2025-02-13 21:42:01 -0700

    ADD: input for player name

--- game.py
+++ game.py
@@ -11,4 +11,10 @@ to navigate through a mysterious forest.
 
 # welcome message and introduction
 print("Welcome to the Adventure Game!")  
-print("Your journey begins here...")
+print('Your journey begins here...')
+
+# ask for the player's name
+player_name = input("What is your name, adventurer?")
+
+# Concatenate strings to create a personlized message
+print("Welcome, " + player_name + "! Your journey begins now. ")
